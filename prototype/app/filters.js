88a25498525c2917ec55b3bc577c80e8eaa103c2
ordyboii <68291857+ordyboii@users.noreply.github.com>
2024-11-18 11:19:09 +0000

    new prototype demo

---
+++ prototype/app/filters.js
@@ -0,0 +1,11 @@
+//
+// For guidance on how to create filters see:
+// https://prototype-kit.service.gov.uk/docs/filters
+//
+
+// eslint-disable-next-line
+const govukPrototypeKit = require("govuk-prototype-kit");
+// eslint-disable-next-line
+const addFilter = govukPrototypeKit.views.addFilter;
+
+// Add your filters here
