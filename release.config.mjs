bd6521cfc4713334a294cf603c2b5665878744a0
Jake Ord <68291857+ordyboii@users.noreply.github.com>
2024-11-19 12:37:33 +0000

    Fix assets and file copy (#9)
    * fix: pega icons and file copy in vite.config.ts
    
    * swapped folders and files around
    
    * fix: new plugin commands plus fixed sass

--- release.config.mjs
+++ release.config.mjs
@@ -1,3 +1,4 @@
 export default {
+  pkgRoot: "dist",
   branches: ["main", { name: "next", prerelease: true }],
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
+import { copyFile } from "fs/promises";
 
 export default defineConfig({
+  plugins: [
+    {
+      name: "Copy package.json",
+      async closeBundle() {
+        await copyFile(resolve("package.json"), resolve("dist/package.json"));
+        await copyFile(resolve("LICENSE"), resolve("dist/LICENSE"));
+        await copyFile(resolve("README.md"), resolve("dist/README.md"));
+      },
+    },
+  ],
   publicDir: "src/plugin",
   build: {
-    outDir: "aui",
+    outDir: "dist",
     lib: {
       entry: resolve(__dirname, "src/aui-frontend.ts"),
       formats: ["cjs"],
