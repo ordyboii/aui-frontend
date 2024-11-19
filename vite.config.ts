eb23004e1e191a32ca7d0382b46c9e1ce9b9c960
ordyboii <68291857+ordyboii@users.noreply.github.com>
2024-11-19 13:53:43 +0000

    fix: a whole bunch of stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,10 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
-import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
-  plugins: [
-    viteStaticCopy({
-      targets: [
-        {
-          src: "package.json",
-          dest: "",
-        },
-        {
-          src: "LICENSE",
-          dest: "",
-        },
-        {
-          src: "README.md",
-          dest: "",
-        },
-      ],
-    }),
-  ],
-  publicDir: "src/plugin",
+  publicDir: "src/public",
   build: {
-    outDir: "dist",
+    outDir: "package/aui",
     lib: {
       entry: resolve(__dirname, "src/aui-frontend.ts"),
       formats: ["es"],
