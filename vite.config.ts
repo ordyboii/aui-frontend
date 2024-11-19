893b8fe3c28a88a5b58d32eb68cd0209f22a53ef
ordyboii <68291857+ordyboii@users.noreply.github.com>
2024-11-19 12:25:14 +0000

    swapped folders and files around

--- vite.config.ts
+++ vite.config.ts
@@ -5,18 +5,15 @@ import { copyFile } from "fs/promises";
 export default defineConfig({
   plugins: [
     {
-      name: "Copy plugin config",
-      async buildStart() {
-        await copyFile(
-          resolve("src/govuk-prototype-kit.config.json"),
-          resolve("./govuk-prototype-kit.config.json")
-        );
+      name: "Copy package.json",
+      async closeBundle() {
+        await copyFile(resolve("package.json"), resolve("dist/package.json"));
       },
     },
   ],
   publicDir: "src/plugin",
   build: {
-    outDir: "aui",
+    outDir: "dist",
     lib: {
       entry: resolve(__dirname, "src/aui-frontend.ts"),
       formats: ["cjs"],
