6495357fcadad366c6949ec0b799f247522707a8
ordyboii <68291857+ordyboii@users.noreply.github.com>
2024-11-19 12:36:43 +0000

    fix: new plugin commands plus fixed sass

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,8 @@ export default defineConfig({
       name: "Copy package.json",
       async closeBundle() {
         await copyFile(resolve("package.json"), resolve("dist/package.json"));
+        await copyFile(resolve("LICENSE"), resolve("dist/LICENSE"));
+        await copyFile(resolve("README.md"), resolve("dist/README.md"));
       },
     },
   ],
